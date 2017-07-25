ed2344e224e0b401453aa8c68d3efd8d096b702a
Yangyan Li <yangyan.lee@gmail.com>
2017-07-24 23:00:59 -0700

    Fix std.

--- denoise_gan_baseline.py
+++ denoise_gan_baseline.py
@@ -93,6 +93,7 @@ data_dim = data.shape[1]
 split = 66511
 data_real = data[:split, :]
 data_std = np.std(data_real, axis=0)
+data_std_tensor = torch.Tensor(data_std)
 data_tensor_real = torch.Tensor(data_real)
 data_tensor_fake = torch.Tensor(data[split:, :])
 dataset_real = torch.utils.data.TensorDataset(data_tensor_real, torch.Tensor(np.ones(shape=(split))))
@@ -181,7 +182,7 @@ batch_fake = torch.FloatTensor(opt.batch_size, data_dim)
 zeros = torch.FloatTensor(opt.batch_size, data_dim)
 label_real = torch.LongTensor(opt.batch_size)
 label_fake = torch.LongTensor(opt.batch_size)
-standard_deviation = torch.FloatTensor(data_std)
+standard_deviation = torch.FloatTensor(opt.batch_size, data_dim)
 
 if opt.cuda:
     batch_real = batch_real.cuda()
@@ -228,6 +229,7 @@ iter_fake = iter(data_loader_fake)
 for iter_idx in range(1, opt.n_epoch * len(data_loader_real)+1):
     samples_fake, iter_fake = get_next_batch(iter_fake, data_loader_fake)
     batch_fake.data.resize_(samples_fake.size()).copy_(samples_fake)
+    standard_deviation.data.resize_(samples_fake.size()).copy_(data_std_tensor.expand_as(samples_fake))
     residual = generator(batch_fake)*(3*standard_deviation)
     logits_fake = dscrmntor(batch_fake + residual)
 

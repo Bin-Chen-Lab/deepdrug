8a2eb4b80b55d246266b2bd80fe526e9f468369b
Bin Chen Lab @ UCSF <binchenindiana@gmail.com>
2017-07-28 08:36:08 -0700

    add new evaluation method

--- eval/gan_pred_analysis.R
+++ eval/gan_pred_analysis.R
@@ -11,6 +11,7 @@ pre_path = paste( model_root, "/", "pred/", model_id, ".txt", sep="")
 
 pred = read.csv(pre_path, sep="\t", header =F)
 
+#tsne analysis
 load(paste(data_root, "/", "lincs_signatures_cmpd_landmark_all.RData", sep=""))
 good_samples = sample(1:66511, 2000)
 fake_samples = sample(66511:76511, 2000)
@@ -24,3 +25,14 @@ samples_gan = cbind(lincs_signatures[, c(good_samples)], t(pred[fake_samples -66
 tsne_out2 <- Rtsne(t(samples_gan), ) # Run TSNE
 plot(tsne_out2$Y, col = c(rep(1, 1000), rep(3, 1000 )), pch=20) # Plot the result ,col=cell_tumor, pch=rges_bin
 
+#correlation analysis
+
+cors_before_gan = NULL
+cors_after_gan = NULL
+
+mappings = read.csv("mappings_good_bad_id.csv")
+for (i in 1:nrow(mappings)){
+  cors_after_gan = c(cors_after_gan, cor(lincs_signatures[mappings$good_id[id],], pred[mappings$bad_id[i]]))
+  cors_before_gan = c(cors_before_gan, cor(lincs_signatures[mappings$good_id[id],], lincs_signatures[mappings$bad_id[i]]))
+  
+}

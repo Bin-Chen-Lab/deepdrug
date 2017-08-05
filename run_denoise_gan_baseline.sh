4f9c8206eaabfc637ca7e31cd8eab87253d2b949
Yangyan Li <yangyan.lee@gmail.com>
2017-08-05 10:09:59 -0700

    User larger batch size.

--- run_denoise_gan_baseline.sh
+++ run_denoise_gan_baseline.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=3 python3 ./denoise_gan_baseline.py --data_root ../data/ --cuda > log_denoise_gan_baseline.txt 2>&1 &
+CUDA_VISIBLE_DEVICES=3 python3 ./denoise_gan_baseline.py --data_root ../data/ --cuda --batch_size 2048 > log_denoise_gan_baseline.txt 2>&1 &
